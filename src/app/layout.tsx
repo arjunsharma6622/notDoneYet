79574df1c655d19a664b8cd419f260b60af3663a
Arjun Sharma <arjunsharma6622@gmail.com>
2024-08-06 01:31:59 +0530

    adding ogimgs in the env file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,8 +8,31 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Not Done Yet",
-  description: "Grow your Sports Career",
+  title: 'Not Done Yet',
+  description: 'Building the LinkedIn for Sports World. Our app bridges the gap between all sports professionals, creating opportunities for growth, collaboration, and success. Discover How Our Latest Initiative is Transforming the Sports Landscape',
+  icons: [{ rel: 'icon', url: '/favicon.ico' }],
+  openGraph: {
+    title: 'Not Done Yet',
+    description: 'Building the LinkedIn for Sports World. Our app bridges the gap between all sports professionals, creating opportunities for growth, collaboration, and success. Discover How Our Latest Initiative is Transforming the Sports Landscape',
+    url: 'https://notdoneyet.in',
+    images: [
+      {
+        url: process.env.OG_IMAGE || 'https://res.cloudinary.com/dexnb3wk2/image/upload/v1721478008/ndy/og_img.jpg',
+        width: 1200,
+        height: 630,
+        alt: 'Not Done Yet',
+      },
+    ],
+    siteName: 'Not Done Yet',
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Not Done Yet',
+    description: 'Building the LinkedIn for Sports World. Our app bridges the gap between all sports professionals, creating opportunities for growth, collaboration, and success. Discover How Our Latest Initiative is Transforming the Sports Landscape',
+    images: [process.env.OG_IMAGE || 'https://res.cloudinary.com/dexnb3wk2/image/upload/v1721478008/ndy/og_img.jpg'],
+  },
 };
 
 export default function RootLayout({
