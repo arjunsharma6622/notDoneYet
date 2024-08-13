74bdc8c8bcadf51997ebe80abc8b0893ce24b033
Arjun Sharma <arjunsharma6622@gmail.com>
2024-08-13 23:12:18 +0530

    fixed dashboard posts issu3

--- src/utils/getGoogleAuthUrl.ts
+++ src/utils/getGoogleAuthUrl.ts
@@ -4,7 +4,7 @@ export const getGoogleAuthUrl = () => {
     const rootUrl = "https://accounts.google.com/o/oauth2/v2/auth";
 
     const options = {
-        redirect_uri: `${API_HEAD}/auth/google`,
+        redirect_uri: `${process.env.NEXT_PUBLIC_API_HEAD}/auth/google`,
         client_id: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID as string,
         access_type: "offline",
         response_type: "code",
